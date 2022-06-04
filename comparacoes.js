5e65f66c76dfeca3205d23a0967141f6f1b20c6f
Brendon Walefy Oliveira Martins <brendonwalefy@MacBook-Air-de-Brendon.local>
2022-06-04 04:49:14 -0300

    ajuste

--- comparacoes.js
+++ comparacoes.js
@@ -3,8 +3,12 @@
 // == (comparacao implicita) compara somente o valor da variavel
 // === (comparacao explicita) compara tipo e valor da variavel
 
+
+
 const numero = 5;
 const texto = "5";
 
 
 console.log(numero === texto);
+
+
