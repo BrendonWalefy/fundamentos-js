680ceaf0644e7c5b5e465fdd314fa798826f65f2
Brendon Walefy Oliveira Martins <brendonwalefy@MacBook-Air-de-Brendon.local>
2022-06-04 05:09:44 -0300

    exercicio template literal

--- templateLiteral.js
+++ templateLiteral.js
@@ -5,8 +5,16 @@ const nome = "Brendon"
 const idade = 2022 - 1994
 const cidadeDeNascimento = "São Miguel do Araguaia"
 
+const nome = "Leo";
+const idade = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
 //const apresentacao = "Meu nome é " + nome + " Minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
 
 
 const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
 console.log(apresentacao)
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido)
