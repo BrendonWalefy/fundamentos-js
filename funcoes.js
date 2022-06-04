679ebefdc183045ae056358b3347dca448ce2484
Brendon Walefy Oliveira Martins <brendonwalefy@MacBook-Air-de-Brendon.local>
2022-06-04 05:23:24 -0300

    funcoes

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+//let x = "";
+//console.log(x);
+//x = "oi";
+
+// 1) DECLARAÇÃO DE FUNÇÃO
+
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto("Brendon");
+
+// uma funçao chamando a outra
+imprimeTexto(soma());
+
+// tres formas de escrever funçoes com return
+
+
+function soma(){
+    return 2+2;
+}
+//console.log(soma())
+
