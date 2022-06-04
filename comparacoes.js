44ffe9acbd05d6475760a1143b0516736feeddf1
Brendon Walefy Oliveira Martins <brendonwalefy@MacBook-Air-de-Brendon.local>
2022-06-04 04:27:25 -0300

    treinando git e mandando novo arquivo

---
+++ comparacoes.js
@@ -0,0 +1,10 @@
+//comparacoes
+
+// == (comparacao implicita) compara somente o valor da variavel
+// === (comparacao explicita) compara tipo e valor da variavel
+
+const numero = 5;
+const texto = "5";
+
+
+console.log(numero === texto);
