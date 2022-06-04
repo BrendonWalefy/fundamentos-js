1f6731f7a8537e06d175e8c4df61dc4a06df3899
Brendon Walefy Oliveira Martins <brendonwalefy@MacBook-Air-de-Brendon.local>
2022-06-04 04:59:00 -0300

    template literal

---
+++ templateLiteral.js
@@ -0,0 +1,12 @@
+//template literal
+
+
+const nome = "Brendon"
+const idade = 2022 - 1994
+const cidadeDeNascimento = "São Miguel do Araguaia"
+
+//const apresentacao = "Meu nome é " + nome + " Minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+console.log(apresentacao)
